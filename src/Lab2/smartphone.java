8686138fd438bf3a8525a850310cb7abee7b29b2
antonpavperov <antonpavperov@yandex.ru>
2026-01-18 22:10:40 +0300

    Lab2 Task2

---
+++ src/Lab2/smartphone.java
@@ -0,0 +1,19 @@
+package Lab2;
+
+public class smartphone {
+
+    String brand;
+    String model;
+    int memory;
+
+
+    public smartphone(){
+    }
+
+
+    public smartphone(String brand, String model, int memory){
+        this.brand = brand;
+        this.model = model;
+        this.memory = memory;
+    }
+}
